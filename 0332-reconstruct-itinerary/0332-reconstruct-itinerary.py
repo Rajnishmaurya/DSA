85a4018151bae06a47dc6c5931049f16c1ffee38
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-07-17 19:20:52 +0530

    Time: 2 ms (78.45%), Space: 17.8 MB (95.8%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dfs(self,airport):
+        while self.adj_list[airport]:
+            temp=self.adj_list[airport].pop()
+            self.dfs(temp)
+        self.route.append(airport)
+
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        self.route=[]
+        self.adj_list=defaultdict(list)
+
+        for x,y in tickets:
+            self.adj_list[x].append(y)
+        
+        for key in self.adj_list:
+            self.adj_list[key]=sorted(self.adj_list[key],reverse=True)
+        
+        self.dfs("JFK")
+        return self.route[::-1]
+        
