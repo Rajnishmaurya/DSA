93efd9430f0e57573b9074b561a185aaa1d0e06f
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-07-29 00:04:17 +0530

    Time: 399 ms (73.55%), Space: 43.5 MB (96.13%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
@@ -1,16 +1,32 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
-        unique = set()
-        prev = set()
+        unique=set()
+        prev=set()
 
         for num in arr:
-            p = set([num])
+            p=set()
             unique.add(num)
-            for num1 in prev:
-                curr = num1 | num
-                unique.add(curr)
-                p.add(curr)
-            prev = p
-        
+            p.add(num)
+            for temp in prev:
+                x=temp | num
+                unique.add(x)
+                p.add(x)
+            prev=p
         return len(unique)
-        
+"""
+Intuition
+To count all distinct bitwise OR results from subarrays, we process each number and track all OR results from subarrays ending at that index. For each element, we:
+
+Start a new subarray with the current number.
+Extend previous subarrays by OR-ing with the current number.
+Because the OR operation is monotonic, the number of distinct results at each step is limited—OR-ing more elements can't undo bits already set—so the total number of unique values grows slowly.
+
+Complexity
+Time complexity: O(n * 32)
+At each index, we OR the current number with previous OR results.
+The number of distinct OR values per step is bounded by the number of bits in the integers.
+The maximum number in the array is ≤ 10⁹ → fits in 30 bits.
+So we can generate at most 32 distinct OR results per index (worst case).
+Total work: n * 32.
+Space complexity: O(n)
+"""
