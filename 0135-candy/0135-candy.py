53507b07893075c6056495ec00d4141a3e9c85e6
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-08-18 19:15:51 +0530

    Time: 11 ms (90.56%), Space: 20 MB (16.17%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -6,10 +6,11 @@ class Solution:
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
                 result[i]=result[i-1]+1
+        
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
                 if result[i]<=result[i+1]:
                     result[i]=result[i+1]+1
-
+                    
         return sum(result)
         
